4c722e5ea334c48d3a1b0f973075b64c155671da
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-08-25 02:03:45 +0300

    HW13.1-1 full

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import Item
+
+def test_Item_calculate_total_price():
+    test_class = Item('Jho', 100.0, 10)
+    test_data = test_class.calculate_total_price()
+    assert test_data == 1000
+
+def test_apply_discount():
+    test_class = Item('Jho', 100.0, 10)
+    test_data = test_class.apply_discount()
+    assert test_data == None
